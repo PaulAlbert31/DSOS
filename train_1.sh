d6c97a2a2f53791ce6bd34c1f0adfab36fdb2d2f
paul <albert.paul31@gmail.com>
2021-10-14 18:28:52 +0100

    cleanup

--- train_1.sh
+++
@@ -1,9 +0,0 @@
-#Mixup only testmethod
-#CUDA_VISIBLE_DEVICES=1 python main.py --dataset miniimagenet_preset --epochs 100 --batch-size 64 --net preresnet18 --lr 0.1 --steps 50 80 --seed 1 --exp-name ablastud_mix --mixup --noise-ratio 0.3
-
-CUDA_VISIBLE_DEVICES=1 python main.py --dataset miniimagenet_preset --epochs 100 --batch-size 64 --net preresnet18 --lr 0.1 --steps 50 80 --seed 1 --exp-name ablastud_softmix --mixup --noise-ratio 0.3
-
-CUDA_VISIBLE_DEVICES=1 python main.py --dataset miniimagenet_preset --epochs 100 --batch-size 64 --net preresnet18 --lr 0.1 --steps 50 80 --seed 1 --exp-name ablastud_softmixentro --mixup --entro --noise-ratio 0.3
-
-
-
